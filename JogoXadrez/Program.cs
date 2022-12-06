5bdab081890f68d5050163b4cfc856e6dad2a73b
Juliano Candido <julianosgs@yahoo.com.br>
2022-12-06 16:57:14 -0300

    Começando a implementar o tabuleiro

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -11,8 +11,8 @@ namespace JogoXadrez
     {
         static void Main(string[] args)
         {
-            Posicao P = new Posicao(3, 4);
-            Console.WriteLine("Posição: " + P);
+            Tabuleiro tab = new Tabuleiro(8,8);
+            Console.ReadLine();
         }
     }
 }
---
+++ JogoXadrez/tabuleiro/Peca.cs
@@ -0,0 +1,25 @@
+﻿using tabuleiro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qteMovimentos { get; protected set; }
+        public Tabuleiro tabuleiro { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        {
+            this.posicao=posicao;
+            this.cor=cor;
+            this.tabuleiro=tab;
+            this.qteMovimentos=0;
+        }
+    }
+}
