65990f9f70741469d01819d220905d96769084d3
Juliano Candido <julianosgs@yahoo.com.br>
2022-12-09 10:34:03 -0300

    Comecando a colocar peças no tabuleiro

--- JogoXadrez/tabuleiro/Peca.cs
+++ JogoXadrez/tabuleiro/Peca.cs
@@ -9,17 +9,21 @@ namespace tabuleiro
 {
     internal class Peca
     {
+        private Tabuleiro tab;
+
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
         public int qteMovimentos { get; protected set; }
         public Tabuleiro tabuleiro { get; protected set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        public Peca(Cor cor, Tabuleiro tab)
         {
-            this.posicao=posicao;
+            this.posicao=null;
             this.cor=cor;
             tabuleiro=tab;
             qteMovimentos=0;
         }
+
+        
     }
 }
