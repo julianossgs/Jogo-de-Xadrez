d3a78adb215281b6c365a8e34b2b9b1727f8f21a
Juliano Candido <julianosgs@yahoo.com.br>
2022-12-10 09:35:18 -0300

    Melhorando o método de imprimir tabuleiro

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -13,8 +13,9 @@ namespace JogoXadrez
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
                 tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 9));
+                tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
                 tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
+                tab.colocarPeca(new Torre(Cor.Branca,tab),new Posicao(3,5));
 
                 Tela.imprimirTabuleiro(tab);
                 
--- JogoXadrez/Tela.cs
+++ JogoXadrez/Tela.cs
@@ -9,6 +9,7 @@ namespace JogoXadrez
         {
             for (int i = 0; i < tab.Linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.Colunas; j++)
                 {
                     if (tab.peca(i,j) == null)
@@ -17,11 +18,32 @@ namespace JogoXadrez
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        ImprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                     
                 }
                 Console.WriteLine();
+
+            }
+
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        //Método p/ trocar a cor da peça
+        public static void ImprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+                Console.WriteLine(peca);
+            }
+
+            else
+            {
+                ConsoleColor aux= Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
 
             }
         }
